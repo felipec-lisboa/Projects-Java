46e2814f9c1d3e9b9962d288cccf10e287ffbe67
felipec-lisboa <lilfelipecosta@gmail.com>
2025-08-29 00:07:17 -0300

    ex

--- exercicios/exercicio_entrada_de_dados/src/App.java
+++ exercicios/exercicio_entrada_de_dados/src/App.java
@@ -1,5 +1,70 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    Scanner teclado = new Scanner(System.in);
+
+    // int x,y;
+    // int soma =0;
+
+    // System.out.println("Digite um numero: ");
+    // x = teclado.nextInt();
+
+    // System.out.println("Digite outro numero: ");
+    // y = teclado.nextInt();
+
+    // soma = x + y;
+
+    // System.out.printf("Soma do numero %d com o numero %d = %d\n", x,y,soma);
+
+    // teclado.close();
+    //Atividade1
+
+    // int a,b,c,d;
+    // int diferenca = 0;
+
+
+    // System.out.println("Digite um numero: ");
+    // a = teclado.nextInt();
+
+    // System.out.println("Digite um numero: ");
+    // b = teclado.nextInt();
+
+    // System.out.println("Digite um numero: ");
+    // c = teclado.nextInt();
+
+    //  System.out.println("Digite um numero: ");
+    // d = teclado.nextInt();
+
+    // diferenca = (a*b-c*d);
+
+    // System.out.println("Diferença = "+diferenca);
+
+
+
+    // teclado.close();
+    //atividade 3 
+
+    // int nFuncionario, nHoras;
+    // double valorHora, valorTotal = 0;
+
+    // System.out.println("Digite seu numero: ");
+    // nFuncionario = teclado.nextInt();
+
+    // System.out.println("Digite seu numero de horas: ");
+    // nHoras = teclado.nextInt();
+
+    // System.out.println("Digite o valor da sua hora: ");
+    // valorHora = teclado.nextDouble();
+
+    // System.out.println("Seu numero é: "+nFuncionario);
+    
+    // valorTotal = valorHora * nHoras;
+
+    // System.out.printf("O valor que você tem a receber é: %.2f\n", valorTotal);
+    //ex4
+
     }
+
+    
 }
